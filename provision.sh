9c44ca25fa358558f8a5683f4e5230df42ac6780
Tim Schindler <tim@giantswarm.io>
2016-05-16 04:30:07 +0200

    fixed dashboard config

--- provision.sh
+++ provision.sh
@@ -9,10 +9,11 @@ fleetctl start grafana/grafana.service
 fleetctl start traefik/traefik.service
 
 # Wait until grafana is up.
-while [ "$(curl -s http://admin:admin@grafana.anna.local/api/dashboards/home | jq .meta.isHome)" != "true" ]; do
+curl --output /dev/null --silent --fail http://admin:admin@grafana.anna.local/api/dashboards/home
+while [ $? -ne ]; do
   sleep 1
 done
 
 # Create data sources and dashboards as soon as grafana is up.
-curl -H "Content-Type:application/json" -d @/tmp/anna.local/grafana/data_source.json http://admin:admin@grafana.anna.local/api/api/datasources
+curl -H "Content-Type:application/json" -d @/tmp/anna.local/grafana/data_source.json http://admin:admin@grafana.anna.local/api/datasources
 curl -H "Content-Type:application/json" -d @/tmp/anna.local/grafana/dashboard.json http://admin:admin@grafana.anna.local/api/dashboards/db
